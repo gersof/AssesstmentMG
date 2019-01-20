66a670fbb72f0a351cd2724f756527afa14eaca2
Juan Esteban Angulo uribe <jangulorama@gmail.com>
2019-01-20 11:26:34 -0500

    Removed refrences  that won't be use

--- MasGlobal.Assessment.UI/Controllers/EmployeeController.cs
+++ MasGlobal.Assessment.UI/Controllers/EmployeeController.cs
@@ -3,7 +3,6 @@ using MasGlobal.Assessment.Entities.APIS.Response;
 using Newtonsoft.Json.Serialization;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Net.Http.Formatting;
