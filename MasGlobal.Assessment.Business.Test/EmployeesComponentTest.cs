5a2853a53047f879c91df52a235f65eb8165e7aa
Juan Esteban Angulo uribe <jangulorama@gmail.com>
2019-01-20 22:08:41 -0500

    Added  unit basic testing by 3 process

---
+++ MasGlobal.Assessment.Business.Test/EmployeesComponentTest.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Configuration;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MasGlobal.Assessment.Business.Test
+{
+    [TestClass]
+    public class EmployeesComponentTest
+    {
+        public EmployeesComponentTest()
+        {
+            ConfigurationManager.AppSettings["BaseUrlEmployees"] = "http://masglobaltestapi.azurewebsites.net/api/";
+        }
+
+        [TestMethod]
+        public async Task GetAll_Success()
+        {
+            var result = await new EmployeesComponent().GetAll(null);
+            Assert.IsTrue(result.Count() == 2);
+        }
+
+        [TestMethod]
+        public async Task GetEmployee_Success()
+        {
+            var result = await new EmployeesComponent().GetAll(1);
+            Assert.IsTrue(result.Count() == 1);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception),"Id does not exist.")]
+        public async Task GetEmployee_Fail()
+        {
+            var result = await new EmployeesComponent().GetAll(77);
+        }
+    }
+}
---
+++ MasGlobal.Assessment.Business.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MasGlobal.Assessment.Business.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MasGlobal.Assessment.Business.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d2b2a368-0a7b-4ac9-84f7-fd6e9d094fd9")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
